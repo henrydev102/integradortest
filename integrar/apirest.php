c462ec90504176763ff4c68329a6c42e370e29a7
henrydev102 <137619229+henrydev102@users.noreply.github.com>
2023-06-24 10:07:29 -0300

    Update apirest.php

--- integrar/apirest.php
+++ integrar/apirest.php
@@ -7,71 +7,33 @@ use Slim\Psr7\Response;
 
 require __DIR__ . '/vendor/autoload.php';
 
-// Criação da instância do aplicativo Slim
 $app = AppFactory::create();
 
-// Definir manipuladores para as operações GET, POST, PUT e DELETE
-
-// Rota GET para consultar uma cidade por ID
 $app->get('/cities/{id}', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
-    // Lógica para consultar uma cidade por ID
-    // ...
-
-    // Retorna a cidade como resposta
-    // $response->getBody()->write(json_encode($city));
-    // ...
 
     return $response;
 });
 
-// Rota GET para consultar todas as cidades
 $app->get('/cities', function (ServerRequestInterface $request, ResponseInterface $response) {
-    // Lógica para consultar todas as cidades
-    // ...
-
-    // Retorna todas as cidades como resposta
-    // $response->getBody()->write(json_encode($cities));
-    // ...
-
+ 
     return $response;
 });
 
-// Rota POST para criar uma nova cidade
 $app->post('/cities', function (ServerRequestInterface $request, ResponseInterface $response) {
-    // Lógica para criar uma nova cidade
-    // ...
-
-    // Retorna a cidade recém-criada como resposta
-    // $response->getBody()->write(json_encode($createdCity));
-    // ...
 
     return $response;
 });
 
-// Rota PUT para atualizar uma cidade existente
 $app->put('/cities/{id}', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
-    // Lógica para atualizar uma cidade existente
-    // ...
-
-    // Retorna a cidade atualizada como resposta
-    // $response->getBody()->write(json_encode($updatedCity));
-    // ...
 
     return $response;
 });
 
-// Rota DELETE para excluir uma cidade
 $app->delete('/cities/{id}', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
-    // Lógica para excluir uma cidade
-    // ...
-
-    // Retorna uma resposta adequada (por exemplo, código de status 204 - No Content)
-    // ...
 
     return $response;
 });
 
-// Execução do aplicativo Slim
 $app->run();
 
 ?>
